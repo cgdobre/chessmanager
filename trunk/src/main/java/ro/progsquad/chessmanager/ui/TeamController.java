d88257f4737923a718fb92323bfa776e2acb298f
cgdobre@gmail.com <cgdobre@gmail.com@2668e11f-af40-8109-b2c8-daef13632e30>
2014-01-12 21:34:46 +0000

    DEV - added create from web form for Team. untested.

--- trunk/src/main/java/ro/progsquad/chessmanager/ui/TeamController.java
+++ trunk/src/main/java/ro/progsquad/chessmanager/ui/TeamController.java
@@ -1,13 +1,39 @@
 package ro.progsquad.chessmanager.ui;
+import java.io.IOException;
+import java.text.ParseException;
+
+import org.springframework.roo.addon.web.mvc.controller.finder.RooWebFinder;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ro.progsquad.chessmanager.factories.TeamFactory;
 import ro.progsquad.chessmanager.model.Team;
-import org.springframework.roo.addon.web.mvc.controller.finder.RooWebFinder;
 
 @RequestMapping("/teams")
 @Controller
 @RooWebScaffold(path = "teams", formBackingObject = Team.class)
 @RooWebFinder
 public class TeamController {
+	
+	@RequestMapping(params = { "find=createByTeamName", "form" }, method = RequestMethod.GET)
+    public String createByTeamNameForm(Model uiModel) {
+        return "teams/createByTeamName";
+    }
+    
+    @RequestMapping(params = "find=createByTeamName", method = RequestMethod.GET)
+    public String createByTeamName(@RequestParam("teamName") String teamName, Model uiModel) {
+    	Team team;
+		try {
+			team = TeamFactory.build(teamName, true);
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+			return "teams/list";
+		}
+        
+    	return "redirect:/teams/" + team.getId();
+    }
 }
