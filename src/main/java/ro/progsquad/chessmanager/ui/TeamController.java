65fb8191a01043e13a8536888dd0cbd1e7be65df
cgdobre@gmail.com <cgdobre@gmail.com@2668e11f-af40-8109-b2c8-daef13632e30>
2015-02-04 14:53:45 +0000

    update some dependencies

--- src/main/java/ro/progsquad/chessmanager/ui/TeamController.java
+++ src/main/java/ro/progsquad/chessmanager/ui/TeamController.java
@@ -1,7 +1,6 @@
 package ro.progsquad.chessmanager.ui;
 import java.io.IOException;
 import java.text.ParseException;
-
 import org.springframework.roo.addon.web.mvc.controller.finder.RooWebFinder;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
@@ -9,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import ro.progsquad.chessmanager.factories.TeamFactory;
 import ro.progsquad.chessmanager.model.Team;
 
@@ -18,22 +16,23 @@ import ro.progsquad.chessmanager.model.Team;
 @RooWebScaffold(path = "teams", formBackingObject = Team.class)
 @RooWebFinder
 public class TeamController {
-	
-	@RequestMapping(params = { "find=createByTeamName", "form" }, method = RequestMethod.GET)
+
+    @RequestMapping(params = { "find=createByTeamName", "form" }, method = RequestMethod.GET)
     public String createByTeamNameForm(Model uiModel) {
         return "teams/createByTeamName";
     }
-    
+
     @RequestMapping(params = "find=createByTeamName", method = RequestMethod.GET)
     public String createByTeamName(@RequestParam("teamName") String teamName, Model uiModel) {
-    	Team team;
-		try {
-			team = TeamFactory.build(teamName, true);
-		} catch (IOException | ParseException e) {
-			System.err.println("Error creating team by name " + teamName + ":\n" + e);e.printStackTrace();
-			return "teams/list";
-		}
+        Team team;
+        try {
+            team = TeamFactory.build(teamName, true);
+        } catch (IOException | ParseException e) {
+            System.err.println("Error creating team by name " + teamName + ":\n" + e);
+            e.printStackTrace();
+            return "teams/list";
+        }
         System.out.println("Finished creating team by name " + teamName);
-    	return "redirect:/teams/" + team.getId();
+        return "redirect:/teams/" + team.getId();
     }
 }
